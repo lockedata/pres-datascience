99f1fe0495c4f4563222097252a030e07f9ae2aa
Steph <Steph@itsalocke.com>
2017-09-03 16:30:17 +0100

    Reveal.js path debug

--- buildpres.R
+++ buildpres.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
 slides=sort(list.files("pres","*.Rmd",full.names=TRUE),
             decreasing=TRUE)
-
+system.file("reveal.js-3.3.0.1", package = "revealjs")
 for (f in slides) render(f,output_dir = "docs")
